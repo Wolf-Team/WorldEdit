e74b13251975de1b2270dd831a9e5df7086edb7f
80LK <80lkru@gmail.com>
2021-06-30 19:57:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WorldEdit
+ WorldEdit for InnerCore
---
+++ dev/header.ts
@@ -0,0 +1,12 @@
+/*
+ __        __          _     _ _____     _ _ _   
+ \ \      / /___  _ __| | __| | ____| __| (_) |_ 
+  \ \ /\ / // _ \| '__| |/ _` |  _|  / _` | | __|
+   \ V  V /| (_) | |  | | (_| | |___| (_| | | |_ 
+    \_/\_/  \___/|_|  |_|\__,_|_____|\__,_|_|\__|
+      
+    WorldEdit v1.5 ©WolfTeam
+    GitHub: https://github.com/Wolf-Team
+            https://github.com/Wolf-Team/WorldEdit
+    VK: https://vk.com/wolf___team
+*/
