921585cd4f8bd38d3dea2ed04b06c940c637dcc7
80LK <80lkru@gmail.com>
2021-07-08 01:40:19 +0300

    fix //cut and //paste

--- dev/footer/Buffer.ts
+++ dev/footer/Buffer.ts
@@ -1,4 +1,5 @@
-var Buffer: [number, number, number, Tile][] = [];
+type tBuffer = [number, number, number, Tile][];
+var Buffer: tBuffer = [];
 
 Commands.register({
     name: "//copy",
@@ -41,37 +42,30 @@ Commands.register({
         });
     },
 });
-Commands.register({
+interface CutServerObject {
+    pos: [Vector, Vector];
+    with_air: 0 | 1
+}
+Commands.register<CutServerObject>({
     name: "//cut",
     description: "Cut the selected area.",
     args: "[-a]",
-    call: function (args) {
+    server: function (client, data) {
         runOnMainThread(function () {
-            const world: BlockSource = BlockSource.getCurrentWorldGenRegion();
+            const player = client.getPlayerUid();
+            const world: BlockSource = BlockSource.getDefaultForActor(player);
 
-            const with_air = args.indexOf("-a") != -1 ? true : false;
+            const Buffer: tBuffer = [];
 
-            const pos1 = WorldEdit.getPosition(0);
-            const pos2 = WorldEdit.getPosition(1);
-
-            const end_x = pos1.x > pos2.x ? pos1.x : pos2.x;
-            const start_x = pos1.x > pos2.x ? pos2.x : pos1.x;
-            const end_y = pos1.y > pos2.y ? pos1.y : pos2.y;
-            const start_y = pos1.y > pos2.y ? pos2.y : pos1.y;
-            const end_z = pos1.z > pos2.z ? pos1.z : pos2.z;
-            const start_z = pos1.z > pos2.z ? pos2.z : pos1.z;
-
-            Buffer = [];
-
-            for (let x = start_x; x <= end_x; x++) {
-                for (let y = start_y; y <= end_y; y++) {
-                    for (let z = start_z; z <= end_z; z++) {
+            for (let x = data.pos[0].x; x <= data.pos[1].x; x++) {
+                for (let y = data.pos[0].y; y <= data.pos[1].y; y++) {
+                    for (let z = data.pos[0].z; z <= data.pos[1].z; z++) {
                         const block = world.getBlock(x, y, z);
-                        const coord = Player.getPosition();
+                        const coord = Entity.getPosition(player);
                         coord.x = Math.round(coord.x);
                         coord.y = Math.round(coord.y);
                         coord.z = Math.round(coord.z);
-                        if (block.id == 0 && with_air == false)
+                        if (block.id == 0 && data.with_air == 0)
                             continue;
                         Buffer.push([coord.x - x, coord.y - y, coord.z - z, block]);
                         world.setBlock(x, y, z, 0, 0);
@@ -79,36 +73,62 @@ Commands.register({
                 }
             }
 
-            Game.message(Translation.translate("Region copied."));
+            client.sendMessage(Translation.translate("Region copied."));
+            client.send("worldedit.sendbuffer", Buffer);
         });
+    },
+    call: function (args) {
+        const with_air = args.indexOf("-a") != -1 ? 1 : 0;
+
+        const pos1 = WorldEdit.getPosition(0);
+        const pos2 = WorldEdit.getPosition(1);
+
+        const end_x = pos1.x > pos2.x ? pos1.x : pos2.x;
+        const start_x = pos1.x > pos2.x ? pos2.x : pos1.x;
+        const end_y = pos1.y > pos2.y ? pos1.y : pos2.y;
+        const start_y = pos1.y > pos2.y ? pos2.y : pos1.y;
+        const end_z = pos1.z > pos2.z ? pos1.z : pos2.z;
+        const start_z = pos1.z > pos2.z ? pos2.z : pos1.z;
+
+        return {
+            pos: [
+                { x: start_x, y: start_y, z: start_z },
+                { x: end_x, y: end_y, z: end_z }
+            ],
+            with_air: with_air
+        }
     }
 });
-Commands.register({
+Commands.register<tBuffer>({
     name: "//paste",
     description: "Paste the copied area.",
     args: "",
+    server: function (client, data) {
+        const count = data.length;
+        const player = client.getPlayerUid();
+        const world: BlockSource = BlockSource.getDefaultForActor(player);
+
+        for (let i = 0; i < count; i++) {
+            const coord = Entity.getPosition(player);
+            coord.x = Math.round(coord.x);
+            coord.y = Math.round(coord.y);
+            coord.z = Math.round(coord.z);
+            world.setBlock(coord.x - data[i][0],
+                coord.y - data[i][1],
+                coord.z - data[i][2], data[i][3].id, data[i][3].data);
+        }
+
+        client.sendMessage(
+            Translation.translate(
+                __n(count, "%count% block changed.", "%count% blocks changed.")
+            ).replace("%count%", count.toString())
+        );
+    },
     call: function () {
         const count = Buffer.length;
         if (count == 0) Game.message("Buffer empty");
-        runOnMainThread(function () {
-
-            const world: BlockSource = BlockSource.getCurrentWorldGenRegion();
-
-            for (let i = 0; i < count; i++) {
-                const coord = Player.getPosition();
-                coord.x = Math.round(coord.x);
-                coord.y = Math.round(coord.y);
-                coord.z = Math.round(coord.z);
-                world.setBlock(coord.x - Buffer[i][0],
-                    coord.y - Buffer[i][1],
-                    coord.z - Buffer[i][2], Buffer[i][3].id, Buffer[i][3].data);
-            }
-
-            Game.message(
-                Translation.translate(
-                    __n(count, "%count% block changed.", "%count% blocks changed.")
-                ).replace("%count%", count.toString())
-            );
-        });
+        return Buffer;
     }
 });
+
+Network.addClientPacket("worldedit.sendbuffer", function (data: tBuffer) { Buffer = data; });
